34d40e7dbcae5037a1fa7c345277f1b9c4f67dc9
Esteban Ruiz <esbruiz@outlook.es>
2017-07-04 16:51:37 -0400

    First Commit

---
+++ client/imports/app/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by esteban on 04/07/17.
+ */
+import { Component } from '@angular/core';
+import template from './app.html';
+
+@Component({
+    selector: 'my-app',
+    template
+})
+
+export class MyApp {}
---
+++ client/imports/app/app.module.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by esteban on 04/07/17.
+ */
+import { NgModule, ErrorHandler } from '@angular/core';
+import { MyApp } from './app.component';
+
+@NgModule({
+    declarations: [
+        MyApp
+    ],
+    entryComponents: [
+        MyApp
+    ],
+    providers: [
+        { provide: ErrorHandler }
+    ]
+})
+
+export class AppModule {}
